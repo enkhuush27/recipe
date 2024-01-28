d23afeddf6b5ab86944819d615fbf8a539aa5be4
enkhuush <b.erdenemyagmar@gmail.com>
2024-01-28 19:19:17 +0800

    hailtig delgetsend dursleh

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,38 @@
 import Search from "./model/search";
+import { elements, renderLoader, clearLoader } from "./view/base";
+import * as searchView from "./view/searchView";
 
-let search = new Search("pasta");
+/** web app state
+ * - Hailtin query
+ * - Tuhain uzuulj bga jor
+ * - Likelsn joruud
+ * - Zahialj bgaa jorin nairlaga
+ */
 
-search.doSearch().then((r) => console.log(r));
+const state = {};
+
+const controlSearch = async () => {
+  //Webes hailtin tulhuur ugig olj awna
+  const query = searchView.getInput();
+
+  if (query) {
+    //Shineer hailtin object uusgej ugnu
+    state.search = new Search(query);
+    //Hailt hiihed zoriulj delgetsiin UI beltgene
+    searchView.clearSearch();
+    searchView.clearSearchResult();
+    renderLoader(elements.searchResultDiv);
+    //Hailtiig guitsetgene
+    await state.search.doSearch();
+    //Hailtiin ur dung delgetsend uzuulne
+    clearLoader();
+    if (state.search.result === undefined) alert("hailtaar ilertsgu");
+    else searchView.renderRecipes(state.search.result);
+    console.log(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
